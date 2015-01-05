80b3ff3adc89983a340c3de3186ec099195d5f7a
Trevor Merrifield <trevorm42@gmail.com>
2015-01-04 22:19:00 -0500

    command line arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![feature(old_orphan_check)]
+
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+static USAGE: &'static str = "Usage: machete <path>";
+
+#[derive(RustcDecodable, Show)]
+struct Args {
+    arg_path: String,
+}
+
 fn main() {
-    println!("Hello, world!")
+    let args : Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    let path = args.arg_path;
 }
